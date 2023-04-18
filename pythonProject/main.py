924a3518111951a6788cc6b31947ccc643edefdc
Rafael Tiburcio <108090164+RafaelTiburcio@users.noreply.github.com>
2023-04-17 22:33:39 -0300

    python 101

---
+++ pythonProject/main.py
@@ -0,0 +1,12 @@
+idade : int
+salario : float
+nome : str
+sexo : str
+
+idade = 29
+salario = 1800.88
+nome = 'Rafael Tiburcio'
+sexo = 'M'
+
+print(f"O funcionario {nome}, sexo {sexo}, recebe o valor de R$ {salario} e tem {idade} anos.")
+
---
+++ pythonProject/saida de dados.py
@@ -0,0 +1,4 @@
+x : float
+x = 2.3456
+
+print("{:.2f}".format(x))
